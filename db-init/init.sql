6a3a579015a90900706c22f415665448db12d19b
ebadamZA <eb.adam@gmail.com>
2025-04-22 22:10:54 +0200

    sqlfluff config updates

--- db-init/init.sql
+++ db-init/init.sql
@@ -7,10 +7,18 @@ GO
 USE Weather_DB;
 GO
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='WeatherForecasts' AND xtype='U')
+IF NOT EXISTS (
+    SELECT
+        *
+    FROM
+        sysobjects
+    WHERE
+        name = 'WeatherForecasts'
+        AND xtype = 'U'
+)
 BEGIN
     CREATE TABLE WeatherForecasts (
-        Id INT PRIMARY KEY IDENTITY(1,1),
+        Id INT PRIMARY KEY IDENTITY (1, 1),
         Date DATE NOT NULL,
         TemperatureC INT NOT NULL,
         Summary NVARCHAR(100) NULL
