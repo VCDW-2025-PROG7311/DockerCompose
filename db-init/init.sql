98b33473506acf8bf42552387da6b64449bd4cf3
ebadamZA <eb.adam@gmail.com>
2025-04-22 21:52:50 +0200

    fixing sqlfluff linting error

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,19 +1,27 @@
 IF DB_ID('Weather_DB') IS NULL
 BEGIN
     CREATE DATABASE Weather_DB;
-END
+END;
 GO
 
 USE Weather_DB;
 GO
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='WeatherForecasts' AND xtype='U')
+IF NOT EXISTS (
+        SELECT
+            *
+        FROM
+            sysobjects
+        WHERE
+            name = 'WeatherForecasts'
+            AND xtype = 'U'
+    )
 BEGIN
     CREATE TABLE WeatherForecasts (
-        Id INT PRIMARY KEY IDENTITY(1,1),
+        Id INT PRIMARY KEY IDENTITY (1, 1),
         Date DATE NOT NULL,
         TemperatureC INT NOT NULL,
         Summary NVARCHAR(100) NULL
     );
-END
+END;
 GO
